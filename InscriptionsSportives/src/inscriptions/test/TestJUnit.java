54408987070c7a4758d1126b0f52512d5992c5b1
Calintha <43139562+Calintha@users.noreply.github.com>
2018-11-17 18:45:11 +0100

    premier test validé

--- InscriptionsSportives/src/inscriptions/test/TestJUnit.java
+++ InscriptionsSportives/src/inscriptions/test/TestJUnit.java
@@ -1,14 +1,23 @@
 package inscriptions.test;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-
-import inscriptions.getNom;
+import inscriptions.Inscriptions;
+import inscriptions.Personne;
 
 public class TestJUnit
 {
 	@Test
-	public void TestCandidat()
+	public void TestGetNom()
 	{
-		getNom nom = new getNom();
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Personne tony = inscriptions.createPersonne("Tony", "Dent de plomb", "azerty");
+		assertEquals("Tony", tony.getNom());
+	}
+	
+	public void TestSetNom()
+	{
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Personne tony = inscriptions.createPersonne("Tony", "Dent de plomb", "azerty");
+		assertEquals("Tony", tony.setNom(nom));
 	}
 }
